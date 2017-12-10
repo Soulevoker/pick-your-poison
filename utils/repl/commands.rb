0fba33d05704d1b7a7b18726ec043fbf945f8b9a
Kyle Seaton <sanddragon3@verizon.net>
2017-12-09 19:08:54 -0600

    Basic Game Loop and REPL

---
+++ utils/repl/commands.rb
@@ -0,0 +1,18 @@
+class Commands
+
+  def handle_command(command, params)
+    COMMANDS[command].(params)
+  rescue NoMethodError
+    puts 'Command not recognized.'
+  end
+
+  def self.help(params)
+    puts "Help also supports params: #{params}"
+  end
+
+  def self.quit(params)
+    exit
+  end
+
+  COMMANDS = {'help' => method(:help), 'quit' => method(:quit)}
+end
